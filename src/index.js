4947faf821fe470b9f5c465a461b8a69d676b99a
Ulises Sosa <ulisesdevsosa@gmail.com>
2023-02-27 18:33:34 -0300

    Generate the code for the wines tab

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import './style.css';
 import { builder } from './home.js';
 import { setMenu } from './menu.js';
+import { setWine } from './wines';
 import { updateLiItems } from './updateEvents';
 
 const content = document.querySelector('#content');
 builder(content);
 const items = updateLiItems();
 items.menu.addEventListener('click', () => setMenu(content));
+items.drinks.addEventListener('click', () => setWine(content));
--- src/wines.js
+++ src/wines.js
@@ -1,16 +1,17 @@
 import { createNav } from './setUp';
-import Salmon from './salmon.jpg';
-import Asparagous from './asparagus.jpg';
+import White from './white-wine.jpg';
+import Red from './red-wine.jpg';
 import { updateLiItems } from './updateEvents';
-import { setWine } from './wines';
+import { setMenu } from './menu.js';
 
-export function setMenu(content) {
+export function setWine(content) {
   content.textContent = '';
   window.scrollTo(0, 0);
   const nav = createNav();
   const main = document.createElement('div');
   const h3 = document.createElement('h3');
   const dishesOne = document.createElement('div');
+  const dishesTwo = document.createElement('div');
   const containerOne = document.createElement('div');
   const imageOne = new Image();
   const descriptionOne = document.createElement('div');
@@ -23,29 +24,33 @@ export function setMenu(content) {
   const pTwo = document.createElement('p');
   const span = document.createElement('span');
   const liItems = nav.querySelectorAll('li');
-  const menu = nav.querySelector('.menu');
+  const wines = nav.querySelector('.drinks');
 
-  h3.textContent = 'Dishes of the day';
-  imageOne.src = Salmon;
-  h4One.textContent = 'Salmon with vegatables';
+  h3.textContent = 'Recommended Wines';
+  imageOne.src = Red;
+  h4One.textContent = 'Red wine from Scadrial';
   pOne.textContent =
-    'Seasoned with spices selected by the house, an irresistible dish ideal for lunch or dinner';
-  imageTwo.src = Asparagous;
-  h4Two.textContent = 'Steak with asparagous';
+    'Freshly sliced strawberry and lemon aromas. Medium body, firm and silky tannins and a linear finish. Firm and racy.';
+  imageTwo.src = White;
+  h4Two.textContent = 'White wine from Roshar';
   pTwo.textContent =
-    'A steak accompanied with asparagus ideal to combine with a good wine';
-  span.textContent = `Download our full drink' s menu here`;
+    'Opens with a clear, dense, complex and lemony-flavored, finessed and pure nose of great delicacy.';
+  span.textContent = 'Download our full menu here';
 
   main.classList.add('main');
   dishesOne.classList.add('dish');
+  dishesTwo.classList.add('dish');
   containerOne.classList.add('container');
   containerTwo.classList.add('container');
   imageOne.classList.add('image');
   imageTwo.classList.add('image');
+  imageTwo.style.gridColumn = '2 / 3';
+  descriptionTwo.style.gridRow = '1 / 2';
+
   span.classList.add('download');
 
   liItems.forEach((e) => e.classList.remove('selected'));
-  menu.classList.add('selected');
+  wines.classList.add('selected');
 
   nav.style.margin = '0';
 
@@ -58,20 +63,16 @@ export function setMenu(content) {
   containerTwo.appendChild(imageTwo);
   containerTwo.appendChild(descriptionTwo);
   dishesOne.appendChild(containerOne);
-  dishesOne.appendChild(containerTwo);
-
-  const dishesTwo = dishesOne.cloneNode(true);
-  const dishesThree = dishesOne.cloneNode(true);
+  dishesTwo.appendChild(containerTwo);
 
   main.appendChild(h3);
   main.appendChild(dishesOne);
   main.appendChild(dishesTwo);
-  main.appendChild(dishesThree);
   main.appendChild(span);
 
   content.appendChild(nav);
   content.appendChild(main);
   const items = updateLiItems();
   items.home.addEventListener('click', () => window.location.reload());
-  items.drinks.addEventListener('click', () => setWine(content));
+  items.menu.addEventListener('click', () => setMenu(content));
 }
